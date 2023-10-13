8c220bae3c988af95ffe492b487c7c5751229388
Fatih Taş <1330192+fthts@users.noreply.github.com>
2023-10-13 13:32:19 +0300

    Convert pages router example to order based (#9)

--- README.md
+++ README.md
@@ -6,11 +6,11 @@
 
 ### Next.js
 
-> To  `NEXT_PUBLIC_PROJECT_ACCESS_TOKEN` environment variable. You can create `.env.local` file in the root of the project and set the variable there. See `.env.local.example` for an example.
-
 #### App Router
 
 - Example using the App Router can be found [here](./examples/nextjs/storefront-app-router).
+
+- It needs a project access token with Storefront Access type of a project with Product as a Service subscription. You can configure that through  `NEXT_PUBLIC_PROJECT_ACCESS_TOKEN` environment variable. You can create `.env.local` file in the root of the project and set the variable there. See `example.env.local` for an example.
 
 - You can start the example app by running:
 
@@ -22,7 +22,9 @@
 
 #### Pages Router
 
-- Example using the App Router can be found [here](./examples/nextjs/storefront-pages-router).
+- Example using the Pages Router can be found [here](./examples/nextjs/storefront-pages-router).
+
+- It needs a project access token with Storefront Access type of a project with Smart Order-based subscription. You can configure that through  `NEXT_PUBLIC_ORDER_BASED_PROJECT_ACCESS_TOKEN` environment variable. You can create `.env.local` file in the root of the project and set the variable there. See `example.env.local` for an example.
 
 - You can start the example app by running:
 
--- packages/ui-components/src/lib/utils/price.ts
+++ packages/ui-components/src/lib/utils/price.ts
@@ -1,8 +1,40 @@
 
+import { OrderedProductType } from "@firmhouse/firmhouse-sdk";
+
 export function formatCentsToEuros(cents: number, decimalPoints=2): string {
   return `${(cents / 100).toFixed(decimalPoints)}€`;
 }
 
 export function formatCentsWithCurrency(cents:number, currency: string, locale?: string | null, decimalPoints=2): string {
   return Intl.NumberFormat(locale ?? undefined, {currency, style: 'currency', maximumFractionDigits: decimalPoints, minimumFractionDigits: decimalPoints}).format(cents / 100);
+}
+
+export function getOrderedProductInfo(orderedProduct: OrderedProductType, currency = 'EUR', locale?: string | null) {
+    const isPlanBased = orderedProduct.plan?.id !== undefined;
+    const { product, intervalUnitOfMeasure, interval } = orderedProduct;
+    const { productType } = product;
+    const intervalText = productType === 'one_time_purchase' ? '' :  ` / month`;
+    const shippingText = intervalUnitOfMeasure === 'only_once' ? '' : getShippingInterval(orderedProduct);
+    const totalPrice = orderedProduct.totalAmountIncludingTaxCents ?? 0;
+    const formattedPrice = `${formatCentsWithCurrency(totalPrice, currency, locale)}${intervalText}`;
+    const billingAndShipping = isPlanBased ? shippingText : `${shippingText ? `Bills & `: ''}${shippingText}`;
+
+    return {
+        billingAndShipping, 
+        totalPrice: formattedPrice,
+    }
+}
+
+function getShippingInterval(orderedProduct: OrderedProductType) {
+    // If the interval is default just get that from product
+    const {intervalUnitOfMeasure, interval} = orderedProduct.intervalUnitOfMeasure === 'default' ? orderedProduct.product : orderedProduct;
+    if (intervalUnitOfMeasure === 'only_once' || typeof intervalUnitOfMeasure !== 'string' || typeof interval !== 'number') {
+        return 'Ships once';
+    }
+    const prefix = interval === 1 ? 'every' : `every ${interval}`;
+    // interval units are always plural, so we remove the last character
+    const periodName = intervalUnitOfMeasure.slice(0, -1);
+    const suffix = interval === 1 ? '' : 's';
+
+    return `Ships ${prefix} ${periodName}${suffix}`;
 }
