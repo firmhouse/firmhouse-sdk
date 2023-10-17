e1e29469a4503caf5078fd4a019f83fc8342f297
Fatih Tas <fatihtas26@gmail.com>
2023-10-17 12:25:14 +0300

    Fix formatting issues

--- packages/firmhouse-sdk/release.config.js
+++ packages/firmhouse-sdk/release.config.js
@@ -1,32 +1,32 @@
-const name = 'firmhouse-sdk'
-const srcRoot = `packages/${name}`
+const name = 'firmhouse-sdk';
+const srcRoot = `packages/${name}`;
 /**
  * @type {import('semantic-release-plus').GlobalConfig}
  */
 module.exports = {
-    extends: 'release.config.base.js',
-    pkgRoot: `dist/${srcRoot}`,
-    tagFormat: `${name}-v\${version}`,
-    commitPaths: [`${srcRoot}/*`],
-    plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        [
-            '@semantic-release/changelog',
-            {
-                changelogFile: `${srcRoot}/CHANGELOG.md`
-            }
-        ],
-        '@semantic-release/npm',
-        [
-            '@semantic-release/git',
-            {
-                assets: [`${srcRoot}/package.json`, `${srcRoot}/CHANGELOG.md`],
-                message:
-                    `release(version): Release ${name}` +
-                    // eslint-disable-next-line no-template-curly-in-string
-                    '${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-            }
-        ]
-    ]
+  extends: 'release.config.base.js',
+  pkgRoot: `dist/${srcRoot}`,
+  tagFormat: `${name}-v\${version}`,
+  commitPaths: [`${srcRoot}/*`],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: `${srcRoot}/CHANGELOG.md`,
+      },
+    ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: [`${srcRoot}/package.json`, `${srcRoot}/CHANGELOG.md`],
+        message:
+          `release(version): Release ${name}` +
+          // eslint-disable-next-line no-template-curly-in-string
+          '${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
 };
