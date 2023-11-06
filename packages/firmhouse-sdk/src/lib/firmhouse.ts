ab4c55354b1df6d3f57664fe4ee441c542508e46
Fatih Tas <fatihtas26@gmail.com>
2023-11-06 14:22:49 +0300

    Fix format errors

--- packages/firmhouse-sdk/src/lib/firmhouse.ts
+++ packages/firmhouse-sdk/src/lib/firmhouse.ts
@@ -1,11 +1,11 @@
 import * as Types from './graphql/generated';
-export type * from './resources/BaseResource'
+export type * from './resources/BaseResource';
 export type * from './resources/products';
 export type * from './resources/plans';
 export type * from './resources/subscriptions';
 export * from './graphql/generated';
 export * from './helpers/errors';
 export * from './helpers/types';
-export * from './helpers/GraphQLClient'
+export * from './helpers/GraphQLClient';
 export * from './FirmhouseClient';
 export { Types };
