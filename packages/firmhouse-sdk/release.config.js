a87ad71cf980c71d3ea17e0a3ad8b75e8912b962
Fatih Tas <fatihtas26@gmail.com>
2023-11-07 09:23:58 +0300

    build(firmhouse-sdk): add commitlint, commitizen

--- packages/firmhouse-sdk/release.config.js
+++ packages/firmhouse-sdk/release.config.js
@@ -8,9 +8,10 @@ module.exports = {
   pkgRoot: `dist/${srcRoot}`,
   tagFormat: `${name}-v\${version}`,
   commitPaths: [`${srcRoot}/*`],
+  branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    // Wraps commit-analyzer and release-notes-generator plguins to support squash and merge
+    'semantic-release-unsquash',
     [
       '@semantic-release/changelog',
       {
