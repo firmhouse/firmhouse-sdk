84f45f485567653915f8c250dd5c650fcd8f1855
Fatih Tas <fatihtas26@gmail.com>
2024-03-13 00:40:33 +0300

    docs(firmhouse-sdk): improve TSDoc comments

--- packages/firmhouse-sdk/src/lib/firmhouse.ts
+++ packages/firmhouse-sdk/src/lib/firmhouse.ts
@@ -1,20 +1,4 @@
-import * as Types from './graphql/generated';
-export type * from './resources/BaseResource';
-export type * from './resources/products';
-export type * from './resources/plans';
-export type * from './resources/subscriptions';
-export type * from './resources/carts';
-export type * from './resources/selfServiceCenterToken';
-export type * from './resources/invoices';
-export type * from './resources/projects';
-export type * from './resources/projects';
-export type * from './helpers/subscription';
-export * from './helpers/errors';
-export * from './helpers/types';
-export * from './helpers/GraphQLClient';
-export * from './FirmhouseClient';
-export * from './helpers';
-export {
+import {
   AcceptanceCheckStatus,
   AppliedPromotionDeactivationStrategy,
   AssetStatus,
@@ -36,4 +20,65 @@ export {
   ReturnOrderStatus,
   SubscriptionStatus,
 } from './graphql/generated';
-export { Types };
+export type * from './resources/BaseResource';
+export type * from './resources/products';
+export type * from './resources/plans';
+export type * from './resources/subscriptions';
+export type * from './resources/carts';
+export type * from './resources/selfServiceCenterToken';
+export type * from './resources/invoices';
+export type * from './resources/projects';
+export type * from './resources/projects';
+export type * from './helpers/subscription';
+export * from './helpers/errors';
+export * from './helpers/types';
+export * from './helpers/GraphQLClient';
+export * from './FirmhouseClient';
+export * from './helpers';
+
+const Types = {
+  AcceptanceCheckStatus,
+  AppliedPromotionDeactivationStrategy,
+  AssetStatus,
+  BaseIntervalUnit,
+  BillingCycleIntervalUnit,
+  CollectionCaseStatus,
+  CommitmentUnit,
+  FeedbackTypeEnum,
+  InvoiceStatusEnum,
+  LineItemTypeEnum,
+  MaximumCommitmentUnit,
+  OrderStatus,
+  OrderedProductIntervalUnitOfMeasure,
+  OrderedProductStatus,
+  OrderedProductTypes,
+  PaymentStatusEnum,
+  PaymentTypeEnum,
+  RefundStatus,
+  ReturnOrderStatus,
+  SubscriptionStatus,
+};
+
+export {
+  Types,
+  AcceptanceCheckStatus,
+  AppliedPromotionDeactivationStrategy,
+  AssetStatus,
+  BaseIntervalUnit,
+  BillingCycleIntervalUnit,
+  CollectionCaseStatus,
+  CommitmentUnit,
+  FeedbackTypeEnum,
+  InvoiceStatusEnum,
+  LineItemTypeEnum,
+  MaximumCommitmentUnit,
+  OrderStatus,
+  OrderedProductIntervalUnitOfMeasure,
+  OrderedProductStatus,
+  OrderedProductTypes,
+  PaymentStatusEnum,
+  PaymentTypeEnum,
+  RefundStatus,
+  ReturnOrderStatus,
+  SubscriptionStatus,
+};
